7694fda12412e45ea5d2a16ba7c9f277ad751ad1
bigtyno931128 <bigtyno4@gmail.com>
2023-01-06 11:18:16 +0900

    애플리케이션 bean test

---
+++ core/src/test/java/hellp/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,41 @@
+package hellp.core.beanfind;
+
+import hellp.core.AppConfig;
+import hellp.core.Member.MemberService;
+import hellp.core.Member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean( MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로  조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+         //ac.getBean("XXXX",ㅡ드)
+    }
+}
